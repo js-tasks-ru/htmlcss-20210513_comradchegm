37993d2495da80795beb5aa884947cf51eb98424
AndreyComanche <comradchegm@gmail.com>
2021-06-03 14:24:19 +0500

    add file modal.js

---
+++ components/modal.js
@@ -0,0 +1,17 @@
+(function () {
+        const close = document.getElementById('modal-close');
+        const modal = document.getElementById('modal');
+        const modalOpen = document.getElementById('modal-open');
+
+        if (!close || !modalOpen || !modal) {
+            return;
+        }
+
+        modalOpen.addEventListener('click', () => {
+            modal.style.display = 'flex';
+        });
+
+        close.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+    })();
