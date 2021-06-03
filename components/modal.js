e766d9b2b320c9bb953563c819292de7d736157b
AndreyComanche <comradchegm@gmail.com>
2021-06-03 20:29:53 +0500

    fix modal window for module 6 main page mobile

--- components/modal.js
+++ components/modal.js
@@ -1,17 +1,22 @@
 (function () {
-        const close = document.getElementById('modal-close');
-        const modal = document.getElementById('modal');
-        const modalOpen = document.getElementById('modal-open');
+    const close = document.getElementById('modal-close');
+    const modal = document.getElementById('modal');
+    const modalOpen = document.getElementById('modal-open');
+    const backdrop = document.getElementById('backdrop');
 
-        if (!close || !modalOpen || !modal) {
-            return;
-        }
+    if (!close || !modalOpen || !modal || !backdrop) {
+        return;
+    }
 
-        modalOpen.addEventListener('click', () => {
-            modal.style.display = 'flex';
-        });
+    modalOpen.addEventListener('click', () => {
+        backdrop.style.display = 'block';
+        modal.style.display = 'flex';
+        document.body.style.overflow = 'hidden'
+    });
 
-        close.addEventListener('click', () => {
-            modal.style.display = 'none';
-        });
-    })();
+    close.addEventListener('click', () => {
+        backdrop.style.display = 'none';
+        modal.style.display = 'none';
+        document.body.style.overflow = 'initial'
+    });
+})();
