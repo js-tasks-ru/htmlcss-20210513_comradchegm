a8b7f2ebb3e457d9be6ff9aaf0c2301ada3c4712
AndreyComanche <comradchegm@gmail.com>
2021-06-03 19:22:23 +0500

    add backdrop element

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -2,17 +2,20 @@
     const close = document.getElementById('modal-close');
     const modal = document.getElementById('modal');
     const modalOpen = document.getElementById('modal-open');
+    const backdrop = document.getElementById('backdrop');
 
-    if (!close || !modalOpen || !modal) {
+    if (!close || !modalOpen || !modal || !backdrop) {
         return;
     }
 
     modalOpen.addEventListener('click', () => {
+        backdrop.style.display = 'block';
         modal.style.display = 'flex';
         document.body.style.overflow = 'hidden';
     });
 
     close.addEventListener('click', () => {
+        backdrop.style.display = 'none';
         modal.style.display = 'none';
         document.body.style.overflow = 'initial';
     });
